6b3a07f2c565b978712935070d040e7388143652
karuppasamy-13 <karuppasamymayil13@gmail.com>
2025-10-16 22:34:27 +0530

    patch and delete mapping

--- src/main/java/com/SpringLearning/SpringBootLearning/Model/StudentDTO.java
+++ src/main/java/com/SpringLearning/SpringBootLearning/Model/StudentDTO.java
@@ -20,7 +20,7 @@ public class StudentDTO {
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-g
+
     public String getStudentName() { return studentName; }
     public void setStudentName(String studentName) { this.studentName = studentName; }
 
